a7046dfb52cc70bd1ff41ab2b36c7a9786d227e0
wewee <90890216+weejinyoung@users.noreply.github.com>
2024-01-08 20:39:50 +0900

    Feat: websocket basic setting (#2)

---
+++ src/main/kotlin/smalltalk/backend/WebSocketConfig.kt
@@ -0,0 +1,25 @@
+package smalltalk.backend
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+
+    override fun configureMessageBroker(config: MessageBrokerRegistry) {
+        // /rooms/something 주소를 구독하는 클라이언트에게 메시지를 보낼 수 있게 브로커 활성화
+        config.enableSimpleBroker("/rooms")
+        // /rooms/chat/something 과 같은 주소로 메세지를 보낼 수 있음, @MessageMapping 에선 /somthing 만 mapping 하면 됨
+        config.setApplicationDestinationPrefixes("/rooms/chat")
+    }
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        // ws://domain/ws-connect 주소로 웹 소켓 연결이 가능
+        registry.addEndpoint("/ws-connect")
+    }
+}
