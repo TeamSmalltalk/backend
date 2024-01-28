9efd7f1eaa7ade0f9e7d9e3087d38c5a93f5bf9f
Minuooooo <mmw7741@gmail.com>
2024-01-28 22:12:17 +0900

    feat: Config redis transaction

--- src/main/kotlin/smalltalk/backend/config/redis/RedisConfig.kt
+++ src/main/kotlin/smalltalk/backend/config/redis/RedisConfig.kt
@@ -1,13 +1,19 @@
 package smalltalk.backend.config.redis
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import java.sql.SQLException
+import kotlin.jvm.Throws
 
 @Configuration
+@EnableTransactionManagement  // 다른 DBMS 없이 redis 단독으로 사용
 class RedisConfig(
     @Value("\${spring.data.redis.host}")
     private val host: String,
@@ -19,12 +25,17 @@ class RedisConfig(
     fun redisConnectionFactory() = LettuceConnectionFactory(host, port)
 
     @Bean
+    @Throws(SQLException::class)
+    fun platformTransactionManager() = DataSourceTransactionManagerAutoConfiguration()
+
+    @Bean
     fun redisTemplate(): RedisTemplate<String, String> {
 
         val redisTemplate: RedisTemplate<String, String> = RedisTemplate()
         redisTemplate.keySerializer = StringRedisSerializer()
         redisTemplate.valueSerializer = StringRedisSerializer()
         redisTemplate.connectionFactory = redisConnectionFactory()
+        redisTemplate.setEnableTransactionSupport(true)  // redis transaction
 
         return redisTemplate
     }
