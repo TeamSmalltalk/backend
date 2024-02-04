8639eea72db9f4ae91199939af7118a920ef8fef
weejinyoung <a8118199@gmail.com>
2024-02-05 00:52:30 +0900

    refact: abstract send method in MessageBroker

--- src/main/kotlin/smalltalk/backend/application/implement/message/MessageBroker.kt
+++ src/main/kotlin/smalltalk/backend/application/implement/message/MessageBroker.kt
@@ -1,7 +1,6 @@
 package smalltalk.backend.application.implement.message
 
-import smalltalk.backend.presentation.dto.chatmessage.ChatMessage
 
 interface MessageBroker {
-    fun send(chatRoomId: Long, chatMessage: ChatMessage)
+    fun send(topic: String, message: Any)
 }
