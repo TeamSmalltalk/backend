e17c05aba8f9b87782cff626e350af5684e42f59
wewee <90890216+weejinyoung@users.noreply.github.com>
2024-02-06 14:23:44 +0900

    Feat #6 Websocket (#7)
    * feat: send message to subscriber
    
    * env: import logging library
    
    * refact: abstract send method in MessageBroker
    
    * env: add origin and sockjs setting
    
    * refact: rename
    
    chatting -> chat
    
    * etc: rename

--- src/main/kotlin/smalltalk/backend/presentation/controller/ChatRoomController.kt
+++ src/main/kotlin/smalltalk/backend/presentation/controller/ChatRoomController.kt
@@ -3,16 +3,16 @@ package smalltalk.backend.presentation.controller
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
-import smalltalk.backend.presentation.dto.chatmessage.ChatMessage
 import smalltalk.backend.application.service.chatroom.ChatRoomService
+import smalltalk.backend.presentation.dto.chatmessage.ChatMessage
 
 @Controller
 class ChatRoomController (
-    private val chattingService: ChatRoomService
+    private val chatRoomService: ChatRoomService
 ) {
 
     @MessageMapping("/{roomId}")
-    fun send(@DestinationVariable roomId: String, chattingMessage: ChatMessage) {
-        chattingService.send(roomId, chattingMessage)
+    fun send(@DestinationVariable("roomId") roomId: String, chatMessage: ChatMessage) {
+        chatRoomService.send(roomId, chatMessage)
     }
 }
