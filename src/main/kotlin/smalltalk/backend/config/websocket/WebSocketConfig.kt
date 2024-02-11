6b07b6716ebb2a54b651e51136044e63b908c99d
Minuooooo <mmw7741@gmail.com>
2024-02-11 21:48:19 +0900

    Squashed commit of the following:
    commit 70d7fcbbcbf8b80b8c99c5f4468a3613587943db
    Author: wewee <90890216+weejinyoung@users.noreply.github.com>
    Date:   Sun Feb 11 17:48:03 2024 +0900
    
        Feat: add websocket inbound channel interceptor (#10)
    
        * feat: add websocket inbound channel interceptor
    
        for now we can handle the message came from client to server when before and after the message are arrived
    
        * refact: remove final keyword

--- src/main/kotlin/smalltalk/backend/config/websocket/WebSocketConfig.kt
+++ src/main/kotlin/smalltalk/backend/config/websocket/WebSocketConfig.kt
@@ -1,15 +1,21 @@
 package smalltalk.backend.config.websocket
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.client.standard.StandardWebSocketClient
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.messaging.WebSocketStompClient
 
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+class WebSocketConfig (
+    private val stompHandler: StompHandler
+): WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(config: MessageBrokerRegistry) {
         // /rooms/something 주소를 구독하는 클라이언트에게 메시지를 보낼 수 있게 브로커 활성화
@@ -23,4 +29,14 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-connect").setAllowedOrigins("*")
         registry.addEndpoint("/ws-connect").setAllowedOrigins("*").withSockJS()
     }
+
+    // 메시지가 클라이언트로부터 들어올 때 호출
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(stompHandler)
+    }
+
+    @Bean
+    fun webSocketClient() : WebSocketStompClient =
+        WebSocketStompClient(StandardWebSocketClient())
 }
+
