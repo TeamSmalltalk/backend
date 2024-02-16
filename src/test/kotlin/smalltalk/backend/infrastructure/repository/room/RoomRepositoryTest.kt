e631aff78f3de69262a46df684285d0820062fd5
Minuooooo <mmw7741@gmail.com>
2024-02-16 22:02:15 +0900

    test: find two room by room id

--- src/test/kotlin/smalltalk/backend/infrastructure/repository/room/RoomRepositoryTest.kt
+++ src/test/kotlin/smalltalk/backend/infrastructure/repository/room/RoomRepositoryTest.kt
@@ -15,32 +15,43 @@ import smalltalk.backend.support.redis.RedisTestConfig
 internal class RoomRepositoryTest (
     private val roomRepository: RoomRepository
 ): ExpectSpec({
-
     val logger = KotlinLogging.logger {  }
 
-    afterContainer {
-
-        roomRepository.deleteAll()
-        logger.info { "Delete all room" }
-    }
-
     context("채팅방을 2개 저장할 경우") {
-
         val firstRoomName = "안녕하세요~"
         val secondRoomName = "반가워요!"
 
-        expect("첫 번째 채팅방 id를 반환한다.") {
-
+        expect("첫 번째 채팅방 id를 반환한다") {
             val firstSavedRoomId = roomRepository.save(firstRoomName)
-
             firstSavedRoomId shouldBe 1L
         }
 
-        expect("두 번째 채팅방 id를 반환한다.") {
-
+        expect("두 번째 채팅방 id를 반환한다") {
             val secondSavedRoomId = roomRepository.save(secondRoomName)
-
             secondSavedRoomId shouldBe 2L
         }
+    }
+
+    context("채팅방을 2개 조회할 경우") {
+        val firstRoomName = "안녕하세요~"
+        val secondRoomName = "siuuuuuu"
+        val firstSavedRoomId = roomRepository.save(firstRoomName)
+        val secondSavedRoomId = roomRepository.save(secondRoomName)
+
+        expect("첫 번째 채팅방을 조회한다") {
+            val firstFoundRoom = roomRepository.findById(firstSavedRoomId!!)
+            firstFoundRoom?.id shouldBe firstSavedRoomId
+            firstFoundRoom?.name shouldBe firstRoomName
+        }
+
+        expect("두 번째 채팅방을 조회한다") {
+            val secondFoundRoom = roomRepository.findById(secondSavedRoomId!!)
+            secondFoundRoom?.id shouldBe secondSavedRoomId
+            secondFoundRoom?.name shouldBe secondRoomName
+        }
+    }
+
+    afterContainer {
+        roomRepository.deleteAll()
     }
 })
