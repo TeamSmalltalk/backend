c68da19e8bf8f961740e86aad60c0166906bff3f
Minuooooo <mmw7741@gmail.com>
2024-02-17 17:35:18 +0900

    fix: rename test case

--- src/test/kotlin/smalltalk/backend/infrastructure/repository/room/RoomRepositoryTest.kt
+++ src/test/kotlin/smalltalk/backend/infrastructure/repository/room/RoomRepositoryTest.kt
@@ -26,29 +26,26 @@ internal class RoomRepositoryTest (
 
     context("채팅방을 저장할 경우") {
         val roomName = "Team small talk"
-
-        expect("첫 번째로 저장된 채팅방 id를 반환한다") {
+        expect("입력 받은 채팅방 이름을 통해 저장된 채팅방의 id를 반환한다") {
             val savedRoomId = roomRepository.save(roomName)
             savedRoomId shouldBe 4L
         }
     }
 
     context("채팅방을 채팅방 id로 조회할 경우") {
-
-        expect("첫 번째로 저장된 채팅방을 반환한다") {
+        expect("id가 1이면 일치하는 채팅방을 반환한다") {
             val firstFoundRoom = roomRepository.findById(1L)
             firstFoundRoom?.id shouldBe 1L
             firstFoundRoom?.name shouldBe "안녕하세요~"
         }
-
-        expect("존재하지 않는 채팅방이면 null 값을 반환한다") {
+        expect("id가 일치하는 채팅방이 없으면 null 값을 반환한다") {
             val foundRoom = roomRepository.findById(4L)
             foundRoom.shouldBeNull()
         }
     }
 
     context("모든 채팅방을 조회할 경우") {
-        expect("모든 채팅방을 반환한다") {
+        expect("채팅방이 1개 이상 존재하면 모든 채팅방을 반환한다") {
             roomRepository.findAll().size shouldBe 3
         }
     }
