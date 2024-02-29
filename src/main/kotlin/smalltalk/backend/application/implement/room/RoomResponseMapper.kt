4e85d562a2d28389a8b8dc0dd2c97675725d5615
Minuooooo <mmw7741@gmail.com>
2024-02-29 18:04:21 +0900

    feat: mapping to open response

--- src/main/kotlin/smalltalk/backend/application/implement/room/RoomResponseMapper.kt
+++ src/main/kotlin/smalltalk/backend/application/implement/room/RoomResponseMapper.kt
@@ -1,7 +1,13 @@
 package smalltalk.backend.application.implement.room
 
 import org.springframework.stereotype.Component
+import smalltalk.backend.presentation.dto.room.openResponse
 
 @Component
 class RoomResponseMapper {
+    fun toOpenResponse(roomId: Long, memberId: Long) =
+        openResponse(
+            roomId,
+            memberId
+        )
 }
