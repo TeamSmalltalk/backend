a2ae319fbb9ce6448abae6a2711b4fbe1d1b0e50
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-08-08 17:12:19 +0900

    feat: logging message in outbound channel interceptor

--- src/main/kotlin/smalltalk/backend/config/websocket/OutboundChannelInterceptor.kt
+++ src/main/kotlin/smalltalk/backend/config/websocket/OutboundChannelInterceptor.kt
@@ -3,6 +3,9 @@ package smalltalk.backend.config.websocket
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.messaging.Message
 import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.SimpMessageType
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
 import org.springframework.messaging.support.ChannelInterceptor
 import org.springframework.stereotype.Component
 
@@ -10,8 +13,16 @@ import org.springframework.stereotype.Component
 class OutboundChannelInterceptor: ChannelInterceptor {
 
     private val logger = KotlinLogging.logger {}
+    companion object {
+        private const val SIMP_MESSAGE_TYPE_KEY = "simpMessageType"
+    }
 
     override fun preSend(message: Message<*>, channel: MessageChannel): Message<*>? {
+        if (StompHeaderAccessor.wrap(message).command == StompCommand.RECEIPT)
+            logger.info { "Receipt $message" }
+        if (message.headers[SIMP_MESSAGE_TYPE_KEY] == SimpMessageType.MESSAGE) {
+            logger.info { "Message $message" }
+        }
         return message
     }
 }
