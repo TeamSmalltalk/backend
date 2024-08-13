a0ab8c0618f785ff60edb04f4ffabb9408e9a1a0
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-08-13 23:53:28 +0900

    test: classify by context in room repository

--- src/test/kotlin/smalltalk/backend/apply/infrastructure/repository/room/RoomRepositoryTest.kt
+++ src/test/kotlin/smalltalk/backend/apply/infrastructure/repository/room/RoomRepositoryTest.kt
@@ -2,11 +2,10 @@ package smalltalk.backend.apply.infrastructure.repository.room
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.kotest.core.spec.style.ExpectSpec
-import io.kotest.matchers.booleans.shouldBeFalse
-import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.collections.shouldBeEmpty
-import io.kotest.matchers.collections.shouldBeOneOf
-import io.kotest.matchers.ints.shouldBeZero
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldNotContain
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
@@ -17,6 +16,7 @@ import smalltalk.backend.infrastructure.repository.room.RedisRoomRepository
 import smalltalk.backend.infrastructure.repository.room.RoomRepository
 import smalltalk.backend.support.redis.RedisContainerConfig
 import smalltalk.backend.support.redis.RedisTestConfig
+import smalltalk.backend.support.spec.afterRootTest
 
 @ActiveProfiles("test")
 @SpringBootTest(
@@ -28,43 +28,33 @@ class RoomRepositoryTest(
 ) : ExpectSpec({
     val logger = KotlinLogging.logger { }
 
-    context("채팅방을 저장할 경우") {
+    context("채팅방 저장") {
         val roomName = NAME
-        expect("입력 받은 채팅방 이름을 통해 저장된 채팅방을 반환한다") {
+        expect("저장된 채팅방을 반환한다") {
             val savedRoom = roomRepository.save(roomName)
             savedRoom.run {
                 id shouldBe 1L
                 name shouldBe NAME
-                idQueue.size shouldBe 9
-                members.size shouldBe 1
+                idQueue shouldHaveSize 9
+                members shouldHaveSize 1
             }
         }
     }
 
-    context("채팅방을 id로 조회할 경우") {
-        roomRepository.save(NAME)
-        expect("id가 1L이면 일치하는 채팅방을 반환한다") {
-            val foundRoom = roomRepository.findById(1L)
-            foundRoom?.run {
-                name shouldBe NAME
-                idQueue.size shouldBe 9
-                members.size shouldBe 1
-            }
-        }
-        expect("id가 일치하는 채팅방이 없으면 null 값을 반환한다") {
-            roomRepository.findById(1L).shouldBeNull()
-        }
-    }
-
-    context("모든 채팅방을 조회할 경우") {
+    context("채팅방 조회") {
         repeat(3) {
             roomRepository.save("채팅방$it")
         }
-        expect("채팅방이 1개 이상 존재하면 모든 채팅방을 반환한다") {
-            roomRepository.findAll().size shouldBe 3
+        expect("id와 일치하는 채팅방을 조회한다") {
+            val foundRoom = roomRepository.findById(1L)
+            foundRoom?.run {
+                name shouldBe "채팅방0"
+                idQueue shouldHaveSize 9
+                members shouldHaveSize 1
+            }
         }
-        expect("채팅방이 존재하지 않는다면 비어있는 리스트를 반환한다") {
-            roomRepository.findAll().size.shouldBeZero()
+        expect("모든 채팅방을 조회한다") {
+            roomRepository.findAll() shouldHaveSize 3
         }
     }
 
@@ -73,8 +63,8 @@ class RoomRepositoryTest(
         expect("추가된 memberId를 반환한다") {
             val memberId = roomRepository.addMember(savedRoom)
             roomRepository.findById(savedRoom.id)?.run {
-                idQueue.contains(memberId).shouldBeFalse()
-                members.last() shouldBe memberId
+                idQueue.shouldNotContain(memberId)
+                members.shouldContain(memberId)
             }
         }
     }
@@ -83,35 +73,36 @@ class RoomRepositoryTest(
         val savedRoom = roomRepository.save(NAME)
         val memberIdToDelete = 1L
         expect("일치하는 memberId를 삭제한다") {
-            roomRepository.deleteMember(savedRoom, memberIdToDelete)
-            roomRepository.findById(savedRoom.id)?.run {
-                idQueue.last() shouldBe 1L
-                members.shouldBeEmpty()
+            roomRepository.run {
+                deleteMember(savedRoom, memberIdToDelete)
+                findById(savedRoom.id)?.run {
+                    idQueue.shouldContain(memberIdToDelete)
+                    members.shouldNotContain(memberIdToDelete)
+                }
             }
         }
     }
 
     context("채팅방 삭제") {
-        val savedRoom = roomRepository.save(NAME)
-        expect("일치하는 채팅방을 삭제한다") {
-            roomRepository.deleteByRoom(savedRoom)
-            roomRepository.findById(savedRoom.id).shouldBeNull()
+        val savedRooms =
+            (0 until 3).map {
+                roomRepository.save("채팅방$it")
+            }.toList()
+        expect("정보와 일치하는 채팅방을 삭제한다") {
+            roomRepository.run {
+                deleteByRoom(savedRooms.last())
+                findById(3L).shouldBeNull()
+            }
         }
-    }
-
-    context("모든 채팅방을 삭제할 경우") {
-        repeat(3) {
-            roomRepository.save("채팅방$it")
-        }
-        expect("채팅방이 1개 이상 존재하면 모든 채팅방을 삭제한다") {
+        expect("모든 채팅방을 삭제한다") {
             roomRepository.run {
                 deleteAll()
-                findAll().size.shouldBeZero()
+                findAll().shouldBeEmpty()
             }
         }
     }
 
-    afterEach {
+    afterRootTest {
         roomRepository.deleteAll()
     }
 })
