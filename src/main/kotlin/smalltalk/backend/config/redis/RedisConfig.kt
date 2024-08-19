3a90a5fd3175e01214f879edf052619f3422b3c8
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-08-20 00:19:58 +0900

    feat: config redis template

--- src/main/kotlin/smalltalk/backend/config/redis/RedisConfig.kt
+++ src/main/kotlin/smalltalk/backend/config/redis/RedisConfig.kt
@@ -6,12 +6,15 @@ import org.redisson.config.Config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
     @Value("\${spring.redis.host}")
     private lateinit var host: String
-
     @Value("\${spring.redis.port}")
     private lateinit var port: String
 
@@ -22,4 +25,15 @@ class RedisConfig {
                 useSingleServer().setAddress("redis://$host:$port")
             }
         )
+
+    @Bean
+    fun redisConnectFactory() = LettuceConnectionFactory(host, port.toInt())
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: LettuceConnectionFactory) =
+        RedisTemplate<String, Any>().apply {
+            keySerializer = StringRedisSerializer()
+            valueSerializer = GenericJackson2JsonRedisSerializer()
+            connectionFactory = redisConnectionFactory
+        }
 }
