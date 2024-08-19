40185f1e23a59a7fd024c961c0f667cc8b5cff83
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-08-19 21:32:38 +0900

    fix: github error

---
+++ src/main/kotlin/smalltalk/backend/infra/repository/room/RoomRepository.kt
@@ -0,0 +1,13 @@
+package smalltalk.backend.infra.repository.room
+
+import smalltalk.backend.domain.room.Room
+
+interface RoomRepository {
+    fun save(roomName: String): Room
+    fun findById(roomId: Long): Room?
+    fun findAll(): List<Room>
+    fun deleteById(roomId: Long)
+    fun deleteAll()
+    fun addMember(room: Room): Long
+    fun deleteMember(room: Room, memberId: Long)
+}
