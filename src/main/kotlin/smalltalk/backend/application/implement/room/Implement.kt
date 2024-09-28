dc7e3f755d7010b8569d253c2ec15c5edb2b2dad
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-09-28 10:59:01 +0900

    feat: delete room implement layer

--- src/main/kotlin/smalltalk/backend/application/implement/room/Implement.kt
+++
@@ -1,9 +0,0 @@
-package smalltalk.backend.application.implement.room
-
-import org.springframework.stereotype.Component
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-@Component
-annotation class Implement(val value: String = "")
