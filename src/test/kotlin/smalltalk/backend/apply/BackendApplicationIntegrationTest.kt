73609e942dfa33cd864faf4248b0f870443b3581
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-09-29 15:22:42 +0900

    test: apply kotest fun spec

--- src/test/kotlin/smalltalk/backend/apply/BackendApplicationIntegrationTest.kt
+++ src/test/kotlin/smalltalk/backend/apply/BackendApplicationIntegrationTest.kt
@@ -1,17 +1,16 @@
 package smalltalk.backend.apply
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.context.annotation.Import
-import org.springframework.http.HttpStatus.*
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus.OK
 import org.springframework.test.annotation.DirtiesContext
 import smalltalk.backend.BackendApplication
 import smalltalk.backend.infra.repository.room.RoomRepository
@@ -19,22 +18,18 @@ import smalltalk.backend.presentation.dto.room.request.OpenRequest
 import smalltalk.backend.presentation.dto.room.response.OpenResponse
 import smalltalk.backend.presentation.dto.room.response.SimpleInfoResponse
 import smalltalk.backend.support.redis.RedisContainerConfig
+import smalltalk.backend.support.spec.afterRootTest
 
-@SpringBootTest(
-    classes = [BackendApplication::class],
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest(classes = [BackendApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(RedisContainerConfig::class)
 @DirtiesContext
-class BackendApplicationIntegrationTest {
-    @Autowired
-    lateinit var template: TestRestTemplate
-    @Autowired
-    lateinit var roomRepository: RoomRepository
+class BackendApplicationIntegrationTest(
+    private val roomRepository: RoomRepository,
+    private val template: TestRestTemplate
+) : FunSpec({
     val logger = KotlinLogging.logger { }
 
-    @Test
-    fun `채팅방 생성 요청에 대하여 응답으로 생성된 채팅방과 멤버 정보가 반환된다`() {
+    test("채팅방 생성 요청에 대하여 응답으로 생성된 채팅방과 멤버 정보가 반환된다") {
         val response = template.postForEntity<OpenResponse>("/api/rooms", OpenRequest(NAME))
         response.run {
             statusCode shouldBe CREATED
@@ -45,8 +40,7 @@ class BackendApplicationIntegrationTest {
         }
     }
 
-    @Test
-    fun `모든 채팅방을 조회한다`() {
+    test("모든 채팅방을 조회한다") {
         (1..3).map { roomRepository.save(NAME + it) }
         val response = template.getForEntity<List<SimpleInfoResponse>>("/api/rooms")
         response.run {
@@ -55,8 +49,7 @@ class BackendApplicationIntegrationTest {
         }
     }
 
-    @AfterEach
-    fun tearDown() {
+    afterRootTest {
         roomRepository.deleteAll()
     }
-}
+})
