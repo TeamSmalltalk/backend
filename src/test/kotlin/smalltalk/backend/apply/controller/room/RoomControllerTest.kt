63320ae15897092135e5d37f467b56afa8884878
Minwoo Moon <121410579+Minuooooo@users.noreply.github.com>
2024-09-29 11:23:44 +0900

    test: apply kotest to room controller test

--- src/test/kotlin/smalltalk/backend/apply/controller/room/RoomControllerTest.kt
+++ src/test/kotlin/smalltalk/backend/apply/controller/room/RoomControllerTest.kt
@@ -2,9 +2,9 @@ package smalltalk.backend.apply.controller.room
 
 import com.ninjasquad.springmockk.MockkBean
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.kotest.core.spec.style.FunSpec
+import io.mockk.clearAllMocks
 import io.mockk.every
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType.APPLICATION_JSON
@@ -19,69 +19,67 @@ import smalltalk.backend.exception.room.advice.RoomExceptionSituationCode.FULL
 import smalltalk.backend.exception.room.situation.FullRoomException
 import smalltalk.backend.exception.room.situation.RoomNotFoundException
 import smalltalk.backend.presentation.controller.room.RoomController
+import smalltalk.backend.support.spec.afterRootTest
 import smalltalk.backend.util.jackson.ObjectMapperClient
+
+private fun getStringValue(client: ObjectMapperClient, value: Any) = client.getStringValue(value)
 
 @WebMvcTest(RoomController::class)
 @Import(ObjectMapperClient::class)
-class RoomControllerTest {
-    @Autowired
-    lateinit var mockMvc: MockMvc
-    @Autowired
-    lateinit var client: ObjectMapperClient
-    @MockkBean
-    lateinit var roomService: RoomService
+class RoomControllerTest(
+    @MockkBean private val roomService: RoomService,
+    private val mockMvc: MockMvc,
+    private val client: ObjectMapperClient
+) : FunSpec({
     val logger = KotlinLogging.logger { }
 
-    @Test
-    fun `채팅방 생성 요청에 대하여 응답으로 생성된 채팅방과 멤버의 정보가 반환된다`() {
+    test("채팅방 생성 요청에 대하여 응답으로 생성된 채팅방과 멤버의 정보가 반환된다") {
         val response = createOpenResponse()
         every { roomService.open(any()) } returns response
         mockMvc.post("/api/rooms") {
             contentType = APPLICATION_JSON
-            content = getStringValue(createOpenRequest())
+            content = client.getStringValue(createOpenRequest())
         }.andExpect {
             status { isCreated() }
-            content { json(getStringValue(response), true) }
+            content { json(getStringValue(client, response), true) }
         }
     }
 
-    @Test
-    fun `모든 채팅방을 조회한다`() {
+    test("모든 채팅방을 조회한다") {
         val response = createSimpleInfoResponse()
         every { roomService.getSimpleInfos() } returns response
         mockMvc.get("/api/rooms").andExpect {
             status { isOk() }
-            content { json(getStringValue(response), true) }
+            content { json(getStringValue(client, response), true) }
         }
     }
 
-    @Test
-    fun `채팅방 입장 요청에 대하여 응답으로 멤버의 정보가 반환된다`() {
+    test("채팅방 입장 요청에 대하여 응답으로 멤버의 정보가 반환된다") {
         val response = createEnterResponse()
         every { roomService.enter(any()) } returns response
         mockMvc.patch("/api/rooms/$ID").andExpect {
             status { isOk() }
-            content { json(getStringValue(response), true) }
+            content { json(getStringValue(client, response), true) }
         }
     }
 
-    @Test
-    fun `이미 삭제된 채팅방 입장 요청에 대하여 응답으로 에러 코드 601이 반환된다`() {
+    test("이미 삭제된 채팅방 입장 요청에 대하여 응답으로 에러 코드 601이 반환된다") {
         every { roomService.enter(any()) } throws RoomNotFoundException()
         mockMvc.patch("/api/rooms/$ID").andExpect {
             status { isNotFound() }
-            content { json(getStringValue(createErrorResponseWhenEnter(DELETED.code)), true) }
+            content { json(getStringValue(client, createErrorResponseWhenEnter(DELETED.code)), true) }
         }
     }
 
-    @Test
-    fun `가득찬 채팅방 입장 요청에 대하여 응답으로 에러 코드 602가 반환된다`() {
+    test("가득찬 채팅방 입장 요청에 대하여 응답으로 에러 코드 602가 반환된다") {
         every { roomService.enter(any()) } throws FullRoomException()
         mockMvc.patch("/api/rooms/$ID").andExpect {
             status { isBadRequest() }
-            content { json(getStringValue(createErrorResponseWhenEnter(FULL.code)), true) }
+            content { json(getStringValue(client, createErrorResponseWhenEnter(FULL.code)), true) }
         }
     }
 
-    private fun getStringValue(value: Any) = client.getStringValue(value)
-}
+    afterRootTest {
+        clearAllMocks()
+    }
+})
